53e215361de4a4336934d790fc462fe2ce23af0d
benjarmc <benjamin@digitalconquest.mx>
2025-11-20 17:50:54 -0600

    feat: add Logger

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { configService } from './config/config.service';
+import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Registrar interceptor de logging globalmente
+  app.useGlobalInterceptors(new LoggingInterceptor());
 
   // Habilitar CORS para permitir peticiones desde dominios especificos
   app.enableCors({
